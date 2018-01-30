31b40d9180d5d54119f96dc8524ac10377d48686
Simon <simsaschneider@icloud.com>
2018-01-30 15:39:10 +0100

    Added support for POST Requests (AJAX)

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,28 @@
 
 function getAjaxContent(aRequestUrl, aCallback) {
+    var theRequest = createRequestWithCallback(aCallback)
+    theRequest.open("GET", completeUrlForString(aRequestUrl), true);
+    theRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
+    theRequest.send();
+}
+
+function postAjaxContent(aRequestUrl, aCallback, aContentString) {
+    var theRequest = createRequestWithCallback(aCallback);
+    theRequest.open("POST", completeUrlForString(aRequestUrl), true);
+    theRequest.send(aContentString);
+}
+
+
+function createRequestWithCallback(aCallback) {
     var theRequest = new XMLHttpRequest();
-    let theUrl = location.origin + "/" + aRequestUrl;
     theRequest.onreadystatechange = function() {
         if (theRequest.readyState = XMLHttpRequest.DONE) {
             aCallback(theRequest.responseText);
         }
     }
+    return theRequest;
+}
 
-    theRequest.open("GET", theUrl, true);
-    theRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
-
-    theRequest.send();
+function completeUrlForString(aString) {
+    return location.origin + "/" + aString;
 }
