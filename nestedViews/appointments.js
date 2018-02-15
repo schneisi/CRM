79f30a197d6c76d9f5b689f8ff346a914f13faf3
MrAnnonymos4 <niklh1@gmx.de>
2018-02-15 11:21:44 +0100

    added newAppointment.html

---
+++ nestedViews/appointments.js
@@ -0,0 +1,17 @@
+function initializeCustomers() {
+    //TODO: Not Correct. Has to be the content-div, not the container
+    const contentDiv = document.getElementById("contentContainer");
+
+    let theList = new ListGrid();
+    theList.addListGridField(new ListGridField("Titel", function(anObject) {
+        return anObject.value;
+    }));
+    theList.objects.push(new ListGridHelper("Test", "Wert"));
+    let theDiv = document.createElement("div");
+    theDiv.innerHTML = theList.getHtml();
+    contentDiv.appendChild(theDiv);
+}
+
+function addNewCustomerClicked(){
+    navigateToViewWithId("newCustomerForm", false);
+}
--- nestedViews/insurances.js
+++ nestedViews/insurances.js
@@ -1,14 +1,22 @@
+let theList
+
 function initializeInsurances() {
     //TODO: Not Correct. Has to be the content-div, not the container
     const contentDiv = document.getElementById("contentContainer");
 
-    let theList = new ListGrid();
+    theList = new ListGrid();
     theList.addListGridField(new ListGridField("Versicherungen", function (anObject) {
         return anObject.value;
     }));
     theList.objects.push(new ListGridHelper("Test", "Krankenversicherung ..."));
     let theDiv = document.createElement("div");
+ 
     theDiv.innerHTML = theList.getHtml();
     contentDiv.appendChild(theDiv);
 }
+
+function insuranceClicked(anIndex) {
+    let theInsurance = theList.objects[anIndex];
+}
+
 
