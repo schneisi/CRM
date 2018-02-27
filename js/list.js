0bccebe383d9e70f287fbcb58d537ba854bc4146
Simon <simsaschneider@icloud.com>
2018-02-27 13:49:54 +0100

    StaticListView and Customer-View (RO) improved

--- js/list.js
+++ js/list.js
@@ -57,3 +57,69 @@ class ListGridHelper {
         this.value = aValue;
     }
 }
+
+
+
+class StaticList {
+    constructor(anArray) {
+        this.rows = [];
+        this.rowWidths = anArray;
+    }
+
+    addRow(anArray) {
+        let theRow = new StaticListRow(this);
+        this.rows.push(theRow);
+        for (var eachCellIndex = 0; eachCellIndex < anArray.length;eachCellIndex++) {
+            theRow.addCell(anArray[eachCellIndex]);
+        }
+        return this;
+    }
+
+    getHtml() {
+        let theString = '<table class="listTable mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">';
+        for (var eachRowIndex = 0; eachRowIndex < this.rows.length; eachRowIndex++) {
+            theString += this.rows[eachRowIndex].getHtml();
+        }
+        return theString + "</tr>";
+    }
+}
+
+class StaticListRow {
+    constructor(aStaticList) {
+        this.staticList = aStaticList;
+        this.cells = [];
+    }
+
+    addCell(aString) {
+        let theCell = new StaticListCell(this, aString);
+        this.cells.push(theCell);
+    }
+
+    getHtml() {
+        let theString = "<tr>";
+        for (var eachCellIndex = 0; eachCellIndex < this.cells.length; eachCellIndex++) {
+            theString += this.cells[eachCellIndex].getHtml();
+        }
+        return theString + "</tr>";
+    }
+}
+
+class StaticListCell {
+    constructor(aRow, aString) {
+        this.text = aString;
+        this.isLeftAlign = true;
+        this.row = aRow;
+    }
+
+    getHtml() {
+        let theString;
+        if (this.isLeftAlign) {
+            theString = '<td class="mdl-data-table__cell--non-numeric">';
+        } else {
+            theString = "<td>";
+        }
+        return theString + this.text + "</td>";
+    }
+}
+
+
--- nestedViews/customer.js
+++ nestedViews/customer.js
@@ -1,5 +1,41 @@
 
+function initializeReadOnlyCustomer() {
+    console.log("test");
+    const theContentDiv = document.getElementById("content");
+    let theTable = new StaticList(["30%", "70%"]);
+    
+    let theFirstName = "Amy";
+    let theLastName = "Adams";
+    let theGender = "weiblich";
+    let theBirthday = "01.2.1997";
+    let theStreet = "Doheny Rd. 1";
+    let theZip = "72313";
+    let thePlace = "Beverly Hills";
+    let thePhoneNumber = "012345678910";
+    let theMail = "amy@adams.com";
+    let theLastAppointment = "05.01.2018";
+    let theNotes = "Bekannte Schauspielerin";
+
+    theTable
+        .addRow(["Name", theLastName])
+        .addRow(["Vorname", theFirstName])
+        .addRow(["Geschlecht", theGender])
+        .addRow(["Geburtstag", theBirthday])
+        .addRow(["Straße", theStreet])
+        .addRow(["PLZ", theZip])
+        .addRow(["Ort", thePlace])
+        .addRow(["Telefon", thePhoneNumber])
+        .addRow(["E-Mail", theMail])
+        .addRow(["Letzter Termin", theLastAppointment])
+        .addRow(["Abgeschlossene Versicherungen", "<ul id='customerContractsList'><li>Berufsunfähigkeitsversicherung</li><li>KFZ Versicherung</li></ul>"]);
+
+    let theTableDiv = document.createElement("div");
+    theTableDiv.innerHTML = theTable.getHtml();
+    theContentDiv.appendChild(theTableDiv);
+}
+
+
 //Todo get customer destination string
 function mapsButtonClicked() {
-    callGoogleMaps("Konrad Adenauer Stra�e 34 79540", "Karlsruhe");
+    callGoogleMaps("Konrad Adenauer Stra�e 34 79540", "Karlsruhe");
 }
