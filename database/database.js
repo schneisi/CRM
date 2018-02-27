e7357b67417d0c4171f2d31cda456992b4b25ce6
Simon <simsaschneider@icloud.com>
2018-02-27 15:58:36 +0100

    Improved DB-API (Read)

--- database/database.js
+++ database/database.js
@@ -1,7 +1,24 @@
-
+//API
 function writeInDatabase(aPath, aJSONData){
     let theKey = fbDatabase.ref().child(aPath).push().key;
     let theDictionary = {};
     theDictionary[aPath + "/" + theKey] = aJSONData;
     fbDatabase.ref().update(theDictionary);
+}
+
+
+function getDatabaseSnapshot(aPath, aCallback) {
+    var ref = fbDatabase.ref(aPath).once('value').then(function(aSnapshot) {
+        aCallback(aSnapshot);
+    });
+}
+
+
+//Example
+function snapshot() {
+    getDatabaseSnapshot("products", function(aSnapshot) {
+        aSnapshot.forEach(function (aChildSnapshot) {
+            console.log(aChildSnapshot.child("name").val());
+        })
+    });
 }
