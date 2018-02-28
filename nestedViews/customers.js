fdda413d14e575245ffc9fcb024b4888953b367d
Simon <simsaschneider@icloud.com>
2018-02-28 10:41:25 +0100

    Customers List loading from DB

--- nestedViews/customers.js
+++ nestedViews/customers.js
@@ -4,19 +4,23 @@ function initializeCustomers() {
     let theList = new ListGrid();
     theList.addListGridField(new ListGridField("Titel", anObject => anObject.value));
     theList.clickEventSelector = customerClicked;
-    theList.objects.push(new ListGridHelper("Test", "Wert"));
-    let theDiv = document.createElement("div");
-    theDiv.innerHTML = theList.getHtml();
-    contentDiv.appendChild(theDiv);
+
+    getDatabaseSnapshot("customers", function(aSnapshot) {
+        aSnapshot.forEach(function (aChildSnapshot) {
+            theList.objects.push(new ListGridHelper(aChildSnapshot.key, aChildSnapshot.child("lastname").val()));
+        });
+        let theDiv = document.createElement("div");
+        theDiv.innerHTML = theList.getHtml();
+        contentDiv.appendChild(theDiv);
+        hideSpinner();
+    });
 }
 
 function addNewCustomerClicked(){
     navigateToViewWithId("newCustomerForm", false);
 }
 
-function customerClicked(anId){
-    setActionId(anId);
+function customerClicked(anIndex){
+    setActionId(anIndex);
     navigateToViewWithId("customer", false);
 }
-
-
