a675207a934809d7aa7babbb1ca3dca6f88ae370
MrAnnonymos4 <niklh1@gmx.de>
2018-02-28 10:33:59 +0100

    Read insurances from database, modified listGridHelper objects

--- nestedViews/insurances.js
+++ nestedViews/insurances.js
@@ -2,22 +2,30 @@ let theList;
 
 function initializeInsurances() {
     //TODO: Not Correct. Has to be the content-div, not the container
+    
+
     const contentDiv = document.getElementById("contentContainer");
 
     theList = new ListGrid();
-    theList.addListGridField(new ListGridField("Versicherungen", anObject => anObject.value));
+    theList.addListGridField(new ListGridField("Versicherungen", aListGridHelper => aListGridHelper.value));
     theList.clickEventSelector = customerClicked;
-    theList.objects.push(new ListGridHelper("Test", "Krankenversicherung ..."));
-    theList.objects.push(new ListGridHelper("Test", "Lebensversucherung ..."));
     theList.clickEventSelector = insuranceClicked;
-    let theDiv = document.createElement("div");
- 
-    theDiv.innerHTML = theList.getHtml();
-    contentDiv.appendChild(theDiv);
+       getDatabaseSnapshot("products", function (aSnapshot) {
+           aSnapshot.forEach(function (aChildSnapshot) {
+               theList.objects.push(new ListGridHelper(aChildSnapshot.key, aChildSnapshot.child("name").val()));
+           });
+           let theDiv = document.createElement("div");
+
+           theDiv.innerHTML = theList.getHtml();
+           contentDiv.appendChild(theDiv);
+        });
+    
+
 }
 
 function insuranceClicked(anIndex) {
-    setActionId(anIndex);
+    let theHelper = theList.objects[anIndex];
+    setActionId(theHelper.object);
     navigateToViewWithId("insurance", false);
     //let theHelper = theList.objects[anIndex];
     //console.log(theHelper.value)
