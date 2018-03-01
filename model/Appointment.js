3a5fcb3d46dc0326acc5903c2d6d5393f7462b80
Halme96 <halme@dhbw-loerrach.de>
2018-03-01 10:17:15 +0100

    Better Appointment

--- model/Appointment.js
+++ model/Appointment.js
@@ -11,4 +11,16 @@ class Appointment extends BaseDatabaseObject {
     dashboardRepresentation() {
         return this.date().toLocaleString("de-DE") + " " + this.title();
     }
+    street() {
+        return this.getValueOfChild("address/street");
+    }
+    place() {
+        return this.getValueOfChild("address/place");
+    }
+    zip() {
+        return this.getValueOfChild("address/zip");
+    }
+    notes() {
+        return this.getValueOfChild("notes");
+    }
 }
