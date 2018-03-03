739f76cbe51fd8ecfdf867a497012b0c5cc3144b
Simon <simsaschneider@icloud.com>
2018-03-04 00:17:28 +0100

    Appointments in Dashboard improved

--- js/dashboard.js
+++ js/dashboard.js
@@ -18,14 +18,25 @@ class DashboardView extends BaseView{
     
     showNextAppointments() {
         let theCallback = function (aSnapshot) {
+            let theListGrid = new ListGrid();
+            currentView.appointmentListGrid = theListGrid; 
+            theListGrid.showTitle = false;
+            theListGrid.clickEventSelector = currentView.appointmentClicked;
+            theListGrid.addListGridField(new ListGridField("", anAppointment => anAppointment.dateString()));
+            theListGrid.addListGridField(new ListGridField("", anAppointment => anAppointment.title()));
+
             let theAppointmentsList = Appointment.createObjectsFromSnapshot(aSnapshot, Appointment);
             theAppointmentsList.forEach(function (eachAppointment) {
-                let eachLi = document.createElement("li");
-                eachLi.innerHTML = eachAppointment.dashboardRepresentation();
-                eachLi.className= "dashboard"
-                document.getElementById("appointmentList").appendChild(eachLi);
+                theListGrid.objects.push(eachAppointment);
             });
+            document.getElementById("appointmentsDiv").innerHTML = theListGrid.getHtml();
         };
         FbDatabase.getDatabaseSnapshot("appointments", theCallback, "date", FbDatabase.valueForDate(new Date()), null, 3);
+    }
+
+    appointmentClicked(anIndex) {
+        let theAppointment = this.appointmentListGrid.objects[anIndex];
+        setActionId(theAppointment.key());
+        navigateToViewWithId("appointment");
     }
 }
