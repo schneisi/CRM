7f7769a3592a03b6e3c6f9e3118fdc64b921ddd4
Simon <simsaschneider@icloud.com>
2018-03-19 14:56:47 +0100

    Moment refactoring

--- public/model/Customer.js
+++ public/model/Customer.js
@@ -109,8 +109,7 @@ class Customer extends BaseDatabaseObject {
         return this.stringForDate(this.birthday());
     }
     isoBirthdayOnlyString() {
-        let theDateMonth = this.birthday().getMonth() + 1;
-        return this.birthday().getFullYear() + "-" + this.getFullStringForNumber(theDateMonth) + "-" + this.getFullStringForNumber(this.birthday().getDate());
+        return this.isoStringForDate(this.birthday());
     }
 
     mapsApiUrl() {
@@ -153,7 +152,7 @@ class Customer extends BaseDatabaseObject {
                 };
                 let theOptions = {
                     orderChild: "birthday",
-                    startObject: FbDatabase.birthdayStringForDate(new Date()),
+                    startObject: moment().format("MM/DD"),
                     limit: 3
                 };
                 FbDatabase.getDatabaseSnapshot("customers", theBirthdayCallback, null, theOptions);
